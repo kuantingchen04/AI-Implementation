57563b3ac9a6fc26e8805c98df6d5882a23d023b
kevche <kevche@umich.edu>
2018-02-25 11:10:04 -0500

    test valid instead of test unique solution

--- sudoku/test_sudoku.py
+++ sudoku/test_sudoku.py
@@ -1,10 +1,42 @@
 import unittest
 
 from sudoku import load_sudoku, backtrack_search_solver
+import numpy as np
 
 # too hard? might sol might vary?
+def check_sol(val_list):
+    # val_list = ['7', '9', '8', '4', '1', '3', '5', '6', '2', '6', '3', '4', '2', '5', '8', '9', '1', '7',
+    #        '5', '1', '2', '7', '6', '9', '3', '8', '4', '1', '7', '9', '8', '4', '5', '2', '3', '6',
+    #        '4', '8', '6', '3', '2', '1', '7', '9', '5', '2', '5', '3', '9', '7', '6', '1', '4', '8',
+    #        '3', '6', '1', '5', '8', '7', '4', '2', '9', '8', '4', '5', '1', '9', '2', '6', '7', '3',
+    #        '9', '2', '7', '6', '3', '4', '8', '5', '1']
+
+    val_list = [ int(x) for x in val_list ]
+    valid_set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+    val_mat = []
+    for i in range(0,81,9):
+        val_mat.append(val_list[i:i+9])
+
+    val_mat = np.array(val_mat)
+
+    # Check col
+    for j in range(9):
+        if set( val_mat[:,j].flatten() ) != valid_set:
+            return False
+
+    for i in range(9):
+        if set(val_mat[i, :].flatten()) != valid_set:
+            return False
+
+    for i in range(0,9,3):
+        for j in range(0,9,3):
+            if set(val_mat[i:i+3,j:j+3].flatten()) != valid_set:
+                return False
+    return True
 
 class TestSudoku(unittest.TestCase):
+
 
     def test_backtrack_easy(self):
         csp = load_sudoku("tests/suinput_easy.csv")
@@ -13,8 +45,7 @@ class TestSudoku(unittest.TestCase):
         self.assertTrue(result)
 
         val_list = [ val for (key, val) in sorted(result.iteritems()) ]
-        sol = ['7', '9', '8', '4', '1', '3', '5', '6', '2', '6', '3', '4', '2', '5', '8', '9', '1', '7', '5', '1', '2', '7', '6', '9', '3', '8', '4', '1', '7', '9', '8', '4', '5', '2', '3', '6', '4', '8', '6', '3', '2', '1', '7', '9', '5', '2', '5', '3', '9', '7', '6', '1', '4', '8', '3', '6', '1', '5', '8', '7', '4', '2', '9', '8', '4', '5', '1', '9', '2', '6', '7', '3', '9', '2', '7', '6', '3', '4', '8', '5', '1']
-        self.assertEqual(val_list,sol)
+        self.assertTrue(check_sol(val_list))
 
 
     def test_arc_only_very_easy(self):
@@ -24,8 +55,7 @@ class TestSudoku(unittest.TestCase):
         self.assertTrue(result)
 
         val_list = [ val for (key, val) in sorted(result.iteritems()) ]
-        sol = ['3', '8', '6', '7', '9', '2', '5', '1', '4', '9', '4', '1', '5', '3', '8', '7', '6', '2', '5', '2', '7', '1', '4', '6', '3', '8', '9', '1', '7', '4', '9', '6', '3', '2', '5', '8', '8', '5', '2', '4', '1', '7', '9', '3', '6', '6', '3', '9', '2', '8', '5', '4', '7', '1', '4', '9', '3', '8', '5', '1', '6', '2', '7', '7', '1', '5', '6', '2', '9', '8', '4', '3', '2', '6', '8', '3', '7', '4', '1', '9', '5']
-        self.assertEqual(val_list,sol)
+        self.assertTrue(check_sol(val_list))
 
     def test_arc_backtrack_easy(self):
         csp = load_sudoku("tests/suinput_easy.csv")
@@ -34,8 +64,16 @@ class TestSudoku(unittest.TestCase):
         self.assertTrue(result)
 
         val_list = [ val for (key, val) in sorted(result.iteritems()) ]
-        sol = ['7', '9', '8', '4', '1', '3', '5', '6', '2', '6', '3', '4', '2', '5', '8', '9', '1', '7', '5', '1', '2', '7', '6', '9', '3', '8', '4', '1', '7', '9', '8', '4', '5', '2', '3', '6', '4', '8', '6', '3', '2', '1', '7', '9', '5', '2', '5', '3', '9', '7', '6', '1', '4', '8', '3', '6', '1', '5', '8', '7', '4', '2', '9', '8', '4', '5', '1', '9', '2', '6', '7', '3', '9', '2', '7', '6', '3', '4', '8', '5', '1']
-        self.assertEqual(val_list,sol)
+        self.assertTrue(check_sol(val_list))
+
+    def test_mrv_only_easy(self):
+        csp = load_sudoku("tests/suinput_easy.csv")
+
+        result = backtrack_search_solver(csp, False, True)
+        self.assertTrue(result)
+
+        val_list = [ val for (key, val) in sorted(result.iteritems()) ]
+        self.assertTrue(check_sol(val_list))
 
 if __name__ == '__main__':
     unittest.main()
